c40d81fabe2ea3bc350cc5f6594b8568d436e9a1
Nicolas de Guilhem de Lataillade <nicolas.dgdl@hotmail.fr>
2022-03-18 19:13:16 +0100

    main structure done

---
+++ src/components/app.jsx
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import { FaSearch } from 'react-icons/fa';
+import SearchBar from './search_bar';
+
+class App extends Component {
+
+  render() {
+    const iconStyle = { position: 'absolute', top: '70px', left: '60px', fontWeight: '400', fontSize: '2.6em', opacity: '0.4' }
+    return(
+    <div>
+      <div className='left-scene'>
+        <FaSearch style={iconStyle} />
+        <SearchBar />
+        <div className='selected-gif'>
+
+        </div>
+      </div>
+      <div className='right-scene'>
+      </div>
+    </div>
+    )
+  }
+}
+
+export default App;
---
+++ src/components/search_bar.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class SearchBar extends Component {
+  render() {
+    return(
+      <input className="form-control form-search"></input>
+    )
+  }
+}
+
+export default SearchBar;
